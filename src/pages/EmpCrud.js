a34b63f8d9fe352c3fb2c35def6371bd4e17b932
Nimsara Perera <nethum.nimsara294@gmail.com>
2023-08-29 01:35:05 +0530

    Initial front end template completed

---
+++ src/pages/EmpCrud.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, Fragment } from "react";
+import Table from "react-bootstrap/Table";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const EmpCrud = () => {
+  const empdata = [
+    {
+      emp_id: 1,
+      firstname: "Alex",
+      lastname: "Gordan",
+      dep: "IT",
+    },
+    {
+      emp_id: 2,
+      firstname: "Jhon",
+      lastname: "Doe",
+      dep: "HR",
+    },
+    {
+      emp_id: 3,
+      firstname: "Jane",
+      lastname: "Doe",
+      dep: "Financial",
+    },
+    {
+      emp_id: 4,
+      firstname: "Rachel",
+      lastname: "Green",
+      dep: "IT",
+    },
+  ];
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(empdata);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Fragment>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.length > 0
+            ? data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.emp_id}</td>
+                    <td>{item.firstname}</td>
+                    <td>{item.lastname}</td>
+                    <td>{item.dep}</td>
+                    <td colSpan={2}>
+                        <button className="btn btn-primary">Edit</button> &nbsp;
+                        <button className="btn btn-danger">Delete</button>
+                    </td>
+                  </tr>
+                );
+              })
+            : "Loading..."}
+        </tbody>
+      </Table>
+    </Fragment>
+  );
+};
+
+export default EmpCrud;
