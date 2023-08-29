4c30973b5af66851aa2ccaa30d2c25eda4e12d65
Nimsara Perera <nethum.nimsara294@gmail.com>
2023-08-29 17:02:30 +0530

    Added department crud

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Tabs from './Components/Tabs';
+import DepCrud from './pages/DepCrud';
 import EmpCrud from './pages/EmpCrud';
 
 function App() {
   return (
     <div className="App">
+      <Tabs />
       <EmpCrud />
+      <DepCrud />
     </div>
   );
 }
